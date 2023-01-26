90ddcb9a81ae9f25e02f698cf340b6938f5a105a
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-01-25 22:16:27 -0500

    added API routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 require("dotenv").config(); // config() attached all env vars to the process object
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 // express app
 const app = express();
 
-// global middleware
+// middleware
+app.use(express.json()); // automatically parse data in post reqs as JSON
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ mssg: "welcome to the app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => {
