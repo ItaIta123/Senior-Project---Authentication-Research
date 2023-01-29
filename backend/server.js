21f5d3efd479cafbc471afd646e124dc6e799256
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-01-28 21:15:01 -0500

    created mongoDB and connected the server to it

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 require("dotenv").config(); // config() attached all env vars to the process object
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 // express app
 const app = express();
 
 // middleware
-app.use(express.json()); // automatically parse data in post reqs as JSON
+app.use(express.json()); // automatically parse data in incoming post reqs as JSON
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
@@ -15,7 +16,13 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 4000");
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port 4000");
+    });
+  })
+  .catch((err) => console.log(err));
