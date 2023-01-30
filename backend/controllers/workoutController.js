5cda09ff514ef530831d297d4afed2910ea1b2e6
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-01-29 22:48:32 -0500

    added patch and delete contorllers

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,33 +1,33 @@
 const Workout = require("../models/workoutModel");
 const mongoose = require("mongoose");
 
-// get all workout
+// GET all workout
 const getAllWorkouts = async (req, res) => {
   const workouts = await Workout.find({}).sort({ createdAt: -1 }); // get all workouts docs by descending order
 
   res.status(200).json(workouts);
 };
 
-// get a single workout
+// GET a single workout
 const getAWorkout = async (req, res) => {
   const { id } = req.params;
 
-  // change ID type to a type required by mongoose
+  // check ID type
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "id type does not match" });
+    return res.status(404).json({ error: "id type does not match" });
   }
 
   const workout = await Workout.findById(id);
 
   if (!workout) {
-    return res.status(404).json({ error: "No such workout" });
+    return res.status(400).json({ error: "No such workout" });
   }
 
   // success
   res.status(200).json(workout);
 };
 
-// create a new workout
+// CREATE a new workout
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body;
 
@@ -41,12 +41,53 @@ const createWorkout = async (req, res) => {
   }
 };
 
-// delete a workout
+// DELETE a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
 
-// update a workout
+  // check ID type
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "id type does not match" });
+  }
+
+  const workout = await Workout.findOneAndDelete({ _id: id });
+
+  if (!workout) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  // success
+  res.status(200).json(workout);
+};
+
+// UPDATE a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  // check ID type
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "id type does not match" });
+  }
+
+  const workout = await Workout.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!workout) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  // success
+  res.status(200).json(workout);
+};
 
 module.exports = {
   createWorkout,
   getAllWorkouts,
   getAWorkout,
+  deleteWorkout,
+  updateWorkout,
 };
