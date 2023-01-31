6295ada3a17d8772c0a371f27cc177b208aad741
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-01-31 11:33:10 -0500

    tested api req and fixed CORS using proxy in package.json

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
-  return <h2>Home</h2>;
+  const [workouts, setWorkouts] = useState([]);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const workoutsJson = await response.json();
+
+      if (response.ok) {
+        setWorkouts(workoutsJson);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+      </div>
+    </div>
+  );
 };
