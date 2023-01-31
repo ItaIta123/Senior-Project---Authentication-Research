1fde72ed357b2cc8ced38d342e5bc988faa6ebe7
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-01-31 11:13:28 -0500

    started frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NavBar } from "./compnents/NavBar";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
