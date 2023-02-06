53cbbac53ed932b1ea93aba61354e3dc04c7720e
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-02-06 13:30:27 -0500

    added context provider

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,23 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
 
 import { WorkoutDetails } from "../components/WorkoutDetails.js";
-
 import { WorkoutForm } from "../components/WorkoutForm.js";
 
 export const Home = () => {
-  const [workouts, setWorkouts] = useState([]);
+  const { workouts, dispatch } = useWorkoutContext();
+
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch("/api/workouts");
       const workoutsJson = await response.json();
 
       if (response.ok) {
-        setWorkouts(workoutsJson);
+        dispatch({ type: "SET_WORKOUTS", payload: workoutsJson });
       }
     };
 
     fetchWorkouts();
-  }, []);
+  }, [dispatch]);
   return (
     <div className="home">
       <div className="workouts">
