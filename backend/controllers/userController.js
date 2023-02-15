88d20d255810b1b00c5505725c4d24e86dbf753f
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-02-15 14:17:41 -0500

    working on login and sign up

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+const UserModel = require("../models/userModel");
+
+// LOGIN user
+const loginUser = async (req, res) => {
+  res.json({ mssg: "login User" });
+};
+
+// SIGNUP user
+
+const signupUser = async function (req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
+
+module.exports = { loginUser, signupUser };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config(); // config() attached all env vars to the process obj
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workoutsRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 // express app
 const app = express();
@@ -15,6 +16,8 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/workouts", workoutRoutes);
+
+app.use("/api/user", userRoutes);
 
 // connect to db
 mongoose
