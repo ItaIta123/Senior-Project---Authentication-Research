cc1ba49a1e4f65adbe4316f6e0e46e14fe0d76ca
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-02-16 19:57:44 -0500

    created a user context

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,19 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw new Error("All fields must be filled!");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error("Invalid email address");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is too weak");
+  }
+
   // check if user already exists
   const emailTaken = await this.findOne({ email: email });
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { AuthContextProvider } from "../src/context/AuthContext";
 import { WorkoutContextProvider } from "../src/context/WorkoutContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WorkoutContextProvider>
-      <App />
-    </WorkoutContextProvider>
+    <AuthContextProvider>
+      <WorkoutContextProvider>
+        <App />
+      </WorkoutContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
