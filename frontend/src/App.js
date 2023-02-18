ac1a189f9141ff236112af2bf51b78b970e0d9c8
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-02-18 11:03:07 -0500

    added signup functionallity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+
 import { NavBar } from "./components/NavBar";
 function App() {
   return (
@@ -9,6 +12,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { useSignup } from "../hooks/useSignUp";
+
+export const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signup, isLoading, error } = useSignup();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // try to sign up
+    console.log(email, password);
+    await signup(email, password);
+  };
+
+  return (
+    <form className="signup" onSubmit={handleSubmit}>
+      <h3>Sign Up</h3>
+
+      <label>Email: </label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+
+      <button disabled={isLoading}>Sign Up</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
