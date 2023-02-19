3cf2c56145b489825a5e322215e823bafc8eb645
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-02-19 15:35:31 -0500

    added middelware and redirections

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,12 @@
+const requireAuth = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: "Authorization token required" });
+  }
+
+  // check for token
+  next();
+};
+
+module.exports = { requireAuth };
