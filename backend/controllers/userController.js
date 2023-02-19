7155b4ab31f7f945309f3ee195a115dcecc7d6f1
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-02-19 15:35:31 -0500

    added middelware and redirections

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,15 @@ const UserModel = require("../models/userModel");
 
 // LOGIN user
 const loginUser = async (req, res) => {
-  res.json({ mssg: "login User" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.login(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
 };
 
 // SIGNUP user
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -50,5 +50,30 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+// static signup method
+userSchema.statics.login = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw new Error("All fields must be filled!");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error("Invalid email address");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is too weak");
+  }
+
+  // check if user already exists
+  const user = await this.findOne({ email: email });
+
+  if (!user) {
+    throw new Error("Email or password do not match");
+  }
+
+  return user;
+};
+
 // model automatically creates a collection of "Workouts" and let us interact with this collection
 module.exports = mongoose.model("User", userSchema);
